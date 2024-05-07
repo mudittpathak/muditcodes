ede12d73203831990de1042ad43f1c37d454f1e5
Mudit Pathak <119633061+mudittpathak@users.noreply.github.com>
2024-05-07 17:19:51 -0600

    Update page.jsx

--- src/app/studio/index/page.jsx
+++ src/app/studio/index/page.jsx
@@ -14,14 +14,5 @@ import { NextStudio } from 'next-sanity/studio';
 import config from '../../../../sanity.config';
 
 export default function StudioPage({children}) {
-    return (
-        <div>
-            <header>Studio Header</header>
-            <main>
-                <NextStudio config={config} />
-                {children}
-            </main>
-            <footer>Studio Footer</footer>
-        </div>
-    );
+    return <div>{children}</div>
 }
